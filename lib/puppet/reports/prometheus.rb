113ba75e7806028a38a1a8c9d7c41f7eaea277c2
Saurabh D <saurabh.dikshit@puppet.com>
2024-03-14 16:05:29 +0000

    made 70

--- lib/puppet/reports/prometheus.rb
+++ lib/puppet/reports/prometheus.rb
@@ -87,7 +87,7 @@ EOS
     # TYPE puppet_report_example_metric gauge
     EOS
 
-    new_metrics["puppet_report_example_metric{#{common_values.join(',')}}"] = 60
+    new_metrics["puppet_report_example_metric{#{common_values.join(',')}}"] = 70
     
     epochtime = DateTime.now.new_offset(0).strftime('%Q').to_i / 1000.0
     new_metrics["puppet_report{#{common_values.join(',')}}"] = epochtime
